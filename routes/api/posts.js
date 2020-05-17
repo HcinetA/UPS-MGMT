1eea407cab7b7ecb371c44c484b1b1bbd2c6068a
hcineta <medamine.hcinet@digietab.tn>
2020-05-17 20:27:15 +0200

    add comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -209,7 +209,7 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 });
 
-//@router put  api/posts/unlike/:id
+//@router put  api/posts/unlike/p/:id
 //@desc like a post
 //@ access private
 router.put('/unlike/p/:id', pauth, async (req, res) => {
@@ -229,6 +229,103 @@ router.put('/unlike/p/:id', pauth, async (req, res) => {
     post.likesporf.splice(removeIndex, 1);
     await post.save();
     res.json(post.likesporf);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+});
+
+//@router Post api/posts/comment/:id
+//@desc comment on a post
+//@ access private
+
+router.post(
+  '/comment/:id',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      post.comments.unshift(newComment);
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+  }
+);
+//@router Post api/posts/comment/:id
+//@desc comment on a post
+//@ access private
+
+router.post(
+  '/comment/:id',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      post.comments.unshift(newComment);
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+  }
+);
+
+//@router delete api/posts/comment/:id/:comment_id
+//@desc delete comment
+//@ access private
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    //make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(404).json({ msg: 'yser not authorized' });
+    }
+    // get remove index
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('server error');
