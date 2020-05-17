fb8e73847f20a58ac3c7bb14ffc9a22d6b481acb
hcineta <medamine.hcinet@digietab.tn>
2020-05-18 00:36:26 +0200

    Signup&signin implementation

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,54 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>login</div>;
+  const [formData, setFormData] = useState({
+    NI: '',
+    password: '',
+  });
+  const { NI, password } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('SUCCESS');
+  };
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign into Your Account
+      </p>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Numéro inscription'
+            name='NI'
+            value={NI}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='7'
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign up</Link>
+      </p>
+    </Fragment>
+  );
 };
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -41,6 +42,7 @@ const Register = () => {
           <label for='cars'>Classe:</label>
 
           <select name='classe' value={classe} onChange={(e) => onChange(e)}>
+            <option name='cla'></option>
             <option name='Lari1'>Lari1</option>
             <option name='Lari2'>Lari2</option>
             <option name='Larit 3'>Lari3</option>
@@ -82,7 +84,7 @@ const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
